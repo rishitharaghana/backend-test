ab1ce0e8aac2c89c3c2dae4652437cc6d4b86a86
srikrishnameet <srikrishna@meetowner.in>
2025-07-07 11:57:06 +0530

    added the latest changes

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 const express = require('express');
 const cors = require('cors');
 
-
 const propertyRouter = require('./routes/propertyRoutes');
 const userRouter = require('./routes/userRoutes');
 const authRouter = require('./routes/authRoutes');
-const leadsRouter = require('./routes/leadsRoutes')
+const leadsRouter = require('./routes/leadsRoutes');
 
 const app = express();
 
 app.use(express.json());
+
+
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://df01-110-235-236-218.ngrok-free.app', 
+];
+
+
 const corsOptions = {
   origin: function (origin, callback) {
+   
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true,
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  allowedHeaders:
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  exposedHeaders: ["Content-Disposition"],
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, ngrok-skip-browser-warning', // Include ngrok-skip-browser-warning
+  exposedHeaders: ['Content-Disposition'],
 };
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders:
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    credentials: true,
-  })
-);
 
-app.get('/',(req,res)=>{
-    res.send('Hello world');
+app.use(cors(corsOptions));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
 });
 
 
-app.use("/api/v1",propertyRouter);
-app.use("/api/v1",authRouter);
-app.use("/api/v1",userRouter);
-app.use("/api/v1",leadsRouter);
+app.use('/api/v1', propertyRouter);
+app.use('/api/v1', authRouter);
+app.use('/api/v1', userRouter);
+app.use('/api/v1', leadsRouter);
 
-
+// Start server
 const port = 3000;
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
